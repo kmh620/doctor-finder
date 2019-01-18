63994689e5ef5934f2a39e3c7f21e387d8109dba
Katrina Hockman <khockman90@hotmail.com>
2019-01-18 14:54:02 -0800

    Fix syntax errors, attempt to fix api call/parse

--- src/doctor.js
+++ src/doctor.js
@@ -12,12 +12,12 @@ export class Doctor {
   searchForDoctor(doctorName) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let nameUrl = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let nameUrl = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function () {
         if (this.status === 200){
           resolve(request.response);
-          getElements(response);
+
         } else {
           reject(Error(request.statusText));
         }
@@ -26,60 +26,8 @@ export class Doctor {
       request.open("GET", nameUrl, true);
       request.send();
       console.log(request);
+
     });
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-// import { apiKey } from './.env';
-//
-// export class Doctor {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//
-//   promise() {
-//     return new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       const nameUrl = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&location=or-portland&skip=0&limit=10&user_key=${process.env.apiKey}`;
-//
-//
-//       request.onreadystatechange = function() {
-//         if (this.readyState === 4 && this.status === 200) {
-//           response = JSON.parse(this.responseText);
-//           getElements(response);resolve(request.response);
-//         } else {
-//           reject(Error(request.statusText));
-//         }
-//       }
-//
-//       request.open("GET", nameUrl, true);
-//       request.send();
-//
-//   });
-//
-//
-// //     promise.then(function(response) {
-// //      let body = JSON.parse(response);
-// //      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-// //      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-// //    }, function(error) {
-// //      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-// //    });
-// //  });
-// // });
-//
-//
-// }
-//
-//
-// }
--- src/main.js
+++ src/main.js
@@ -10,47 +10,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 $(document).ready(function() {
-   // event.preventDefault();
-
     $("#search-submit").click(function() {
      let doctorName = $("#doctor-search-input").val();
+     $("#doctor-search-input").val();
      console.log(doctorName);
-     // $("#doctor-search-input").val("");
-     let medicalSearchInput = $("#medical-search-input").val();
-     console.log(medicalSearchInput);
 
        let newDoc = new Doctor();
        let docPromise = newDoc.searchForDoctor(doctorName);
 
-       let doctorResult = [];
        let response;
        docPromise.then(function (response) {
          let body = JSON.parse(response);
-         $('#doctor-name').text(`${body.response.name}`);
+         $('#doctor-name').text(`${response.data.profile.first_name}` , `${response.data.profile.last_name}`);
          console.log(body.data.name);
          $("#doctor-patient-status").text(body.data.accepts_new_patients);
          $("#doctor-address").text();
          $("#doctor-phone").text();
          $("#doctor-website").text();
-          }, function(error) {
+       }, function(error) {
           $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-
-
-          const getElements = function(response) {
-              $(".doctor-display").attr("src", response.data[0].images.original.url);
-              console.log(response.data[0].images.original.url);
-            }
-
+        });
+    
     });
-
-     //   let searchResults = response.results;
-     //   let searchOutputHTML = "";
-     //
-     //   searchResults.forEach(function(element) {
-     //     searchOutputHTML += `<li>${element.name}</li>`
-     //   });
-     //   $("#search-results").html(searchOutputHTML);
-     // }
-
-})
-})
+});
